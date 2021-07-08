a0882255e7218f6332cdc75a896135c867090300
Saurav Jha <[saurav.jha@trv.envestnet.com]>
2021-07-08 06:02:36 +0530

    restoring folder name

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import Image from "next/image";
-import Header from "../components/Header";
-import Nav from "../components/Nav";
-import Results from "../components/Results";
+import Header from "../Components/Header";
+import Nav from "../Components/Nav";
+import Results from "../Components/Results";
 import requests from "../Utils/Requests";
 
 export default function Home({ results }) {

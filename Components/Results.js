4fa03efd0b3d44c016fd099c218f86ffddfd7495
Saurav Jha <[saurav.jha@trv.envestnet.com]>
2021-07-08 05:50:08 +0530

    changing folder name

--- Components/Results.js
+++ Components/Results.js
@@ -4,7 +4,7 @@ import FlipMove from "react-flip-move";
 function Results({ results }) {
   return (
     <FlipMove className="px-2 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center">
-      {results.map((item) => (
+      {results?.map((item) => (
         <Thumbnail key={item.id} result={item} />
       ))}
     </FlipMove>
